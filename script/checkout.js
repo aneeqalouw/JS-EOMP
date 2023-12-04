5607e879d34b346fd3ac5833513387d2859dab4a
aneeqalouw <aneeqalouw057@gmail.com>
2023-12-04 15:29:54 +0200

    checkout page no functionality

--- script/checkout.js
+++ script/checkout.js
@@ -1,1 +1,13 @@
 document.querySelector('#currYear').textContent = new Date().getUTCFullYear()
+let cartItems = []
+let cart = document.querySelector('#cartContainer')
+
+if (cartItems == ''){
+    cart.innerHTML = `
+       <p class="my-5">Your cart is empty</p>
+       <a class="fw-bold" href="../html/products.html">Shop now</a>
+    `
+}
+function addToCart(){
+
+}

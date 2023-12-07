8c31c68c6dade81f490875772067bc0d162421a7
aneeqalouw <aneeqalouw057@gmail.com>
2023-12-07 15:25:48 +0200

    website complete without spinner and with some loading bugs

--- script/code.js
+++ script/code.js
@@ -1,1 +1,2 @@
+//getting the current year and displaying it in the span next to the copyright
 document.querySelector('#currYear').textContent = new Date().getUTCFullYear()
--- script/products.js
+++ script/products.js
@@ -1,63 +1,8 @@
+//getting the current year and displaying it in the span next to the copyright
 document.querySelector('#currYear').textContent = new Date().getUTCFullYear()
-let productContainer = document.querySelector('[data-products]')
-let products = [
-  {
-      id: 1,
-      name: 'clear lip gloss',
-      image: 'https://i.postimg.cc/MG4nPyxK/clear.webp',
-      price: 109.90
-  },
-  {
-      id: 2,
-      image: 'https://i.postimg.cc/zfDwZw9C/nude.webp',
-      name: 'nude lip gloss',
-      price: 109.90
-  },
-  {
-      id: 3,
-      image: 'https://i.postimg.cc/jjvWzG3N/red.webp',
-      name: 'red lip gloss',
-      price: 109.90
-  },
-  {
-      id: 4,
-      image: 'https://i.postimg.cc/qv0W9YQz/pink.webp',
-      name: 'pink lip gloss',
-      price: 109.90
 
-  },
-  {
-      id: 5,
-      image: 'https://i.postimg.cc/sfLTYLbt/orange.webp',
-      name: 'peach lip gloss',
-      price: 109.90
-  },
-  {
-      id: 6,
-      image:'https://i.postimg.cc/d3Dvzf9b/pink-Lipoil.webp',
-      name: 'pink lip oil',
-      price: 89.90
-  },
-  {
-      id: 7,
-      image: 'https://i.postimg.cc/htCFqsXg/red-Lipoil.webp',
-      name: 'red lip oil',
-      price: 89.90
-  },
-  {
-      id: 8,
-      image: 'https://i.postimg.cc/nzJGSbnL/plum-Lip-Oil.png',
-      name: 'plum lip oil',
-      price: 89.90
-  },
-  {
-      id: 9,
-      image: 'https://i.postimg.cc/bv3wW1h2/nude-Lipoil.webp',
-      name: 'burgundy lip oil',
-      price: 89.90
-  }
-]
-localStorage.setItem(('products'),JSON.stringify(products))
+let productContainer = document.querySelector('[data-products]')
+let products = JSON.parse(localStorage.getItem('products'))
 let cart = JSON.parse(localStorage.getItem('checkout')) || []
 
 
